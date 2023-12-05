509878594c0c12d5bfa28234eafdbe7272c8585d
rschweit <ronny.schweitzer@gmx.at>
2023-12-05 21:17:12 +0000

    plot_updates

--- plots/bland_altman/script.R
+++ plots/bland_altman/script.R
@@ -1,53 +1,74 @@
 library(tidyverse)
+library(ggplot2)
+library(ggExtra)
+library(gridExtra)
+setwd('')
+colnames(read.csv(''))
 
-#select variables and units
-variable = "SV.BSA_.ml.m2."
-unit='ESV/BSA (ml/m²)'
+plot_list <- list()
+variables <- c("ESV_.ml.","ESV.BSA_.ml.m2.","EDV_.ml.","EDV.BSA_.ml.m2.","SV_.ml.","SV.BSA_.ml.m2.","EF_..." , "CO_.l.min.","CI_.l.min.m2.")             
+units <- c('ESV (ml)','ESV/BSA (ml/m²)','EDV (ml)','EDV/BSA (ml/m²)','SV (ml)','SV/BSA (ml/m²)','EF (%)','CO (l/min)','CI (l/min/m²)')
 
-# Create a nice theme
-theme_update(axis.line.y = element_line(colour="black"),
-             axis.line.x = element_line(colour="black"),
-             axis.text = element_text(colour="black"),
-             panel.grid.major = element_blank(),
-             panel.grid.minor = element_blank(),
-             panel.background = element_blank(),
-             axis.text.x  = element_text(size=22),
-             axis.text.y  = element_text(size=22),
-             axis.title.x  = element_text(size=28,vjust=1, face="plain"),
-             axis.title.y  = element_text(size=28, face = "plain", vjust=1, angle = 90),
-             axis.line = element_line(size = 1.2, linetype = "solid"),
-             axis.ticks = element_line(size = 1), legend.position="none")
+for (i in 1:length(variables)) {
 
-#data import and cleaning
-gated <- read.csv('GenScanII_sax_cine_rest.csv')
-rownames(gated) <- gated$BRU
-gated <- drop_na(select(gated, !!sym(variable)))
+variable = variables[i]
+unit=units[i]
 
-rt <- read.csv('GenScanII_sax_cine_RT_rest.csv',sep=';')
-rownames(rt) <- rt$BRU
-rt <- drop_na(select(rt,!!sym(variable)))
+  # Create a nice theme
+  theme_update(axis.line.y = element_line(colour="black"),
+               axis.line.x = element_line(colour="black"),
+               axis.text = element_text(colour="black"),
+               panel.grid.major = element_blank(),
+               panel.grid.minor = element_blank(),
+               panel.background = element_blank(),
+               axis.text.x  = element_text(size=10),
+               axis.text.y  = element_text(size=10),
+               axis.title.x  = element_text(size=13,vjust=1, face="plain"),
+               axis.title.y  = element_text(size=13, face = "plain", vjust=1, angle = 90),
+               axis.line = element_line(size = 1.2, linetype = "solid"),
+               axis.ticks = element_line(size = 1), legend.position="top")
+  
+  ids <-read.csv()
+  
+  gated <- read.csv()
+  rownames(gated) <- gated$BRU
+  gated <- drop_na(select(gated, !!sym(variable)))
+  
+  rt <- read.csv()
+  rownames(rt) <- rt$BRU
+  rt <- drop_na(select(rt,!!sym(variable)))
+  
+  rt <-rt[rownames(rt) %in% ids$BRU, , drop = FALSE]
+  gated <- gated[rownames(gated) %in% ids$BRU, , drop = FALSE]
+  
+  rt <-rt[rownames(rt) %in% rownames(gated), , drop = FALSE]
+  gated <- gated[rownames(gated) %in% rownames(rt), , drop = FALSE]
+  
+  
+  diff<-unlist(rt[rownames(gated), , drop = FALSE] - gated[rownames(gated), , drop = FALSE])
+  mean <- unlist(unlist(rt[rownames(gated), , drop = FALSE] + gated[rownames(gated), , drop = FALSE])/2)
+  my.data <- data.frame(mean, diff)
+  
+  # pdf("BA/ba_EF.pdf", width=10, height=10)
+  diffplotp <- ggplot(my.data, aes(mean, diff)) + 
+       geom_point(size=1.5, colour = rgb(0,0,0, alpha = 0.5)) + 
+       theme_update() + 
+       geom_hline(yintercept = 0, linetype = 3) +
+       geom_hline(yintercept = mean(my.data$diff)) +
+       geom_hline(yintercept = mean(my.data$diff) + 2*sd(diff), linetype = 2) +
+       geom_hline(yintercept = mean(my.data$diff) - 2*sd(diff), linetype = 2) +
+       ylab(paste(unit, " - Difference")) +
+       xlab("Mean")
+  
+  marginal_plot <- ggMarginal(diffplotp, type="histogram", bins = 25, color= "grey60", fill = "gray90")
 
-rt <-rt[rownames(rt) %in% rownames(gated), , drop = FALSE]
-gated <- gated[rownames(gated) %in% rownames(rt), , drop = FALSE]
+  plot_list[[i]]<-marginal_plot}
 
 
-diff <- unlist(gated - rt)
-mean <- unlist((gated + rt)/2)
-my.data <- data.frame(mean, diff)
 
-#plotting
 
-# pdf("BA/ba_SV_BSA_at_rest_.pdf", width=10, height=10)
-diffplotp <- ggplot(my.data, aes(mean, diff)) + 
-     geom_point(size=2.5, colour = rgb(0,0,0, alpha = 0.5)) + 
-     theme_update() + 
-     geom_hline(yintercept = 0, linetype = 3) +
-     geom_hline(yintercept = mean(my.data$diff)) +
-     geom_hline(yintercept = mean(my.data$diff) + 2*sd(diff), linetype = 2) +
-     geom_hline(yintercept = mean(my.data$diff) - 2*sd(diff), linetype = 2) +
-     ylab(paste(unit, " - Difference")) +
-     xlab("Mean")
-
-marginal_plot <- ggMarginal(diffplotp, type="histogram", bins = 25, color= "grey60", fill = "gray90")
-print(marginal_plot)
-# dev.off()
+grid.arrange(
+  grobs = plot_list,
+  ncol = 3,
+  nrow = 3
+)
--- plots/box_dot_line/script.R
+++ plots/box_dot_line/script.R
@@ -1,85 +1,117 @@
-library (ggplot2)
-library(dplyr)
-library(tidyr)
-df <- read.csv("Desktop/GSII_df_combined.csv")
+library(tidyverse)
+library(gridExtra)
 
-#choose parameters, unit, and expected change
-rest_par <- "EDV.BSA_.ml.m2._rest"
-exercise_par <- "EDV.BSA_.ml.m2._exercise"
-unit <- expression(paste("ESV (ml/" * m^2 * ")"))
-expected_change_up <- TRUE
 
-#gender comparison = 'Sex'; age group comparison = 'decade'
-groups = 'decade'
+setwd()
 
-# Select the specific columns from the existing dataframe df
-df_selected <- df %>% select(BRU, !!sym(groups), !!sym(rest_par), !!sym(exercise_par))
+#read in data (merged resting + exercise data)
+df <- read.csv()
+colnames(df)
 
-# Check for empty/missing values and drop the rows with any empty values
-df_clean <- df_selected %>% drop_na()
+plot_list <- list()
+# Define the lists of parameters
+rest_pars <- c('ESV_.ml._rest',"ESV.BSA_.ml.m2._rest","EDV_.ml._rest","EDV.BSA_.ml.m2._rest", 'SV_.ml._rest',"SV.BSA_.ml.m2._rest","EF_..._rest","CO_.l.min._rest","CI_.l.min.m2._rest")
+exercise_pars <- c('ESV_.ml._exercise',"ESV.BSA_.ml.m2._exercise","EDV_.ml._exercise","EDV.BSA_.ml.m2._exercise", 'SV_.ml._exercise',"SV.BSA_.ml.m2._exercise","EF_..._exercise","CO_.l.min._exercise","CI_.l.min.m2._exercise")
+units <- c(expression(paste("ESV (ml)")),expression(paste("ESV/BSA (ml/m²)")),expression(paste("EDV (ml)")),expression(paste("EDV/BSA (ml/m²)")),expression(paste("SV (ml)")),expression(paste("SV/BSA (ml/m²)")),expression(paste("EF (%)")),expression(paste("CO (l/min)")),expression(paste("CI (l/min/m²)")))
+expected_changes_up <- c(FALSE, FALSE, TRUE, TRUE,TRUE, TRUE,TRUE, TRUE,TRUE)
+groups <- 'decade'
 
-# Combine edv_exercise and edv_rest into a single column called edv, and create another column indicating whether the value came from edv_rest or edv_exercise
-df_final <- df_clean %>%
-  pivot_longer(cols = c(!!sym(rest_par), !!sym(exercise_par)),
-               names_to = "exercise",
-               values_to = "par") %>%
-  mutate(exercise = ifelse(exercise == rest_par , -1, 1))
+# rest_pars <- c("BASELINE.HR","BASELINE.HR")
+# exercise_pars <- c("HR.reached","HR.reached") 
+# units <- c(expression(paste("HR (BPM)")),expression(paste("HR (BPM)")))
+# expected_changes_up <- c(TRUE,TRUE)
+# groupst <- c('decade','Sex')
 
-# Calculate the difference between edv_exercise and edv_rest for each individual
-df_final_diff <- df_final %>%
-  group_by_at(c("BRU", groups)) %>% 
-  summarize(diff = diff(par)) %>%
-  ungroup()
 
-if(groups=='Sex'){
-  df_final <- left_join(df_final, df_final_diff, by = c("BRU", "Sex"))
+# Assuming all lists are of the same length
+for (i in 1:length(rest_pars)) {
+
+  #choose parameters, unit, and expected change
+  rest_par <- rest_pars[i]
+  exercise_par <- exercise_pars[i]
+  unit <- units[i]
+  expected_change_up <- expected_changes_up[i]
   
-  # Define a custom labeller
-  custom_labeller <- function(variable, value) {
-    age_ranges <- c('Women','Men')
-    return(setNames(age_ranges, c('F', 'M')))
+  #gender comparison = 'Sex'; age group comparison = 'decade'
+  # groups <- 'decade'
+  
+  # Select the specific columns from the existing dataframe df
+  df_selected <- df %>% select(BRU, !!sym(groups), !!sym(rest_par), !!sym(exercise_par))
+  
+  # Check for empty/missing values and drop the rows with any empty values
+  df_clean <- df_selected %>% drop_na()
+  
+  # Combine edv_exercise and edv_rest into a single column called edv, and create another column indicating whether the value came from edv_rest or edv_exercise
+  df_final <- df_clean %>%
+    pivot_longer(cols = c(!!sym(rest_par), !!sym(exercise_par)),
+                 names_to = "exercise",
+                 values_to = "par") %>%
+    mutate(exercise = ifelse(exercise == rest_par , -1, 1))
+  
+  # Calculate the difference between edv_exercise and edv_rest for each individual
+  df_final_diff <- df_final %>%
+    group_by_at(c("BRU", groups)) %>% 
+    summarize(diff = diff(par)) %>%
+    ungroup()
+  
+  if(groups=='Sex'){
+    df_final <- left_join(df_final, df_final_diff, by = c("BRU", "Sex"))
+    
+    # Define a custom labeller
+    custom_labeller <- function(variable, value) {
+      age_ranges <- c('Women','Men')
+      return(setNames(age_ranges, c('F', 'M')))
+    }
+    
+    
+  } else{
+    df_final <- left_join(df_final, df_final_diff, by = c("BRU", "decade"))
+    
+    # Define a custom labeller
+    custom_labeller <- function(variable, value) {
+      age_ranges <- c("20-39 years", "40-59 years", "60-79 years")
+      return(setNames(age_ranges, c(2, 4, 6)))
+    }
+  }
+  
+  if (expected_change_up) {
+    color_choice <- c("TRUE" = 'blue', "FALSE" = "red")
+  } else {
+    color_choice <- c("TRUE" = 'red', "FALSE" = "blue")
   }
   
   
-} else{
-  df_final <- left_join(df_final, df_final_diff, by = c("BRU", "decade"))
+  p <- ggplot(df_final, aes(y = par, group = interaction(!!sym(groups), BRU), color = diff > 0)) +
+       geom_line(aes(x = exercise), size = 1, alpha = 0.5) +
+       geom_point(aes(x = exercise), shape = 21, fill = "white", stroke = 1.5, size = 2.5) +
+       geom_boxplot(aes(x = exercise * 2, group = as.factor(exercise)), width = 0.5, color='black') +
+       scale_x_continuous(breaks = c(-1.5, 1.5), labels = c("Rest", "Exercise")) +
+       ylab(unit) +
+       xlab('') +
+       facet_wrap(as.formula(paste0('~', groups)), ncol = 3, labeller = labeller(!!sym(groups) := custom_labeller))+
+       scale_color_manual(values = c(color_choice)) +
+       theme(
+            axis.ticks.x = element_blank(),
+            axis.text = element_text(colour = "black", size = 10),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(size = 1.2, linetype = "solid"),
+            axis.ticks = element_line(size = 1),
+            axis.text.x  = element_text(size = 10, face = "bold"),
+            axis.title = element_text(size = 13, face = "bold"),
+            axis.text.y  = element_text(size = 10),
+            legend.position = "none",
+            strip.background = element_rect(fill = "transparent"),
+            strip.text = element_text(size = 13),
+            panel.border = element_rect(linetype = "solid", color = "black", size = .5, fill = 'transparent'),
+       )
   
-  # Define a custom labeller
-  custom_labeller <- function(variable, value) {
-    age_ranges <- c("20-39 years", "40-59 years", "60-79 years")
-    return(setNames(age_ranges, c(2, 4, 6)))
-  }
-}
+  plot_list[[i]]<-p}
+  
 
-if (expected_change_up) {
-  color_choice <- c("TRUE" = 'blue', "FALSE" = "red")
-} else {
-  color_choice <- c("TRUE" = 'red', "FALSE" = "blue")
-}
-
-p <- ggplot(df_final, aes(y = par, group = interaction(!!sym(groups), BRU), color = diff > 0)) +
-     geom_line(aes(x = exercise), size = 1, alpha = 0.5) +
-     geom_point(aes(x = exercise), shape = 21, fill = "white", stroke = 1.5, size = 2.5) +
-     geom_boxplot(aes(x = exercise * 2, group = as.factor(exercise)), width = 0.5, color='black') +
-     scale_x_continuous(breaks = c(-1.5, 1.5), labels = c("Rest", "Exercise")) +
-     ylab(unit) +
-     xlab('') +
-     facet_wrap(as.formula(paste0('~', groups)), ncol = 3, labeller = labeller(!!sym(groups) := custom_labeller))+
-     scale_color_manual(values = c(color_choice)) +
-     theme(
-          axis.ticks.x = element_blank(),
-          axis.text = element_text(colour = "black", size = 10),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.background = element_blank(),
-          axis.line = element_line(size = 1.2, linetype = "solid"),
-          axis.ticks = element_line(size = 1),
-          axis.text.x  = element_text(size = 15, face = "bold"),
-          axis.title = element_text(size = 15, face = "bold"),
-          axis.text.y  = element_text(size = 12),
-          legend.position = "none",
-          strip.background = element_rect(fill = "transparent"),
-          strip.text = element_text(size = 12),
-          panel.border = element_rect(linetype = "solid", color = "black", size = .5, fill = 'transparent'),
-     )
-print(p)
+grid.arrange(
+  grobs = plot_list,
+  ncol = 3,
+  nrow = 3
+)
